8fae276b7eb20514f5b9cf08686fac0fbe27c010
MichalBarnisin <57236249+MichalBarnisin@users.noreply.github.com>
2020-07-05 23:23:30 +0200

    Efficiency of the Evaluation Algorithm (#2)
    * Do not perform sort, rather use dictionary for rle and compute properties of values in a line when needed
    
    * Rewrite evaluate_line
    remove copying
    update comments
    reformat comments
    remove unused imports
    
    * Add tests for all rules

--- eval.py
+++ eval.py
@@ -1,39 +1,32 @@
 """
-In this file we provide the definition of the
-evaluator class which can be used to assign the
-resulting scores to the board
+In this file we provide the definition of the evaluator class which can be used
+to assign the resulting scores to the board.
 """
 from board import Board
-
-from copy import deepcopy
-from typing import Union
+from typing import Union, List, Any, Dict
 
 
-def rle(data: list):
+def rle(data: List[Any]) -> Dict[Any, int]:
     """
-    Performs run length encoding
+    Performs run length encoding on the data. Does not modify the original data.
+
     :param data: sorted list
-    :return: list with rle pair (number, count)
+    :return: dictionary with keys being the elements of the data and values
+        being the occurrences
     """
-    result = []
-    current = data[0]
-    count = 0
-    for num in data:
-        if num == current:
-            count += 1
+    result = {}
+    for elem in data:
+        if elem in result:
+            result[elem] += 1
         else:
-            result.append((current, count))
-            count = 1
-            current = num
-    result.append((current, count))
+            result[elem] = 1
     return result
 
 
 class Evaluator:
     """
-    In this class, the bonuses are defined as well as
-    the points. Given the FULL Board (i.e. without empty
-    cells), the Evaluator calculates the final score.
+    In this class, the bonuses are defined as well as the points. Given the FULL
+    Board (i.e. without empty cells), the Evaluator calculates the final score.
     """
     DIAGONAL_BONUS = 10
     TWO_OF_A_KIND = 10
@@ -47,44 +40,62 @@ class Evaluator:
     FLUSH_1_10_11_12_13 = 150
 
     @staticmethod
-    def evaluate_line(line: list):
-        line.sort()
-        code = rle(line)
+    def evaluate_line(line: List[int]):
+        """
+        Evaluates a single line of the board. The rules are applied according
+        to the number of different values in the line. Throws an exception if
+        unexpected values or their occurrences are present. Does not modify the
+        original line.
 
+        :param line: single line of the board (row, column or diagonal)
+        :return: score of the line as described in the rules
+        """
+        code = rle(line)
         if len(code) == 5:
-            # flush only or nothing
-            if line == [1, 10, 11, 12, 13]:
+            # Of each value is different, the only combination can be flush,
+            # either straight or <1, 10, 11, 12, 13>
+            if all(x in code for x in [1, 10, 11, 12, 13]):
                 return Evaluator.FLUSH_1_10_11_12_13
-            elif line[-1] - line[0] == 5 - 1:
+            if max(line) - min(line) == 5 - 1:
                 return Evaluator.STRAIGHT_FLUSH
-            else:
-                return 0
+            return 0
+
         elif len(code) == 4:
-            # only double
+            # The only combination with four different values is a single pair,
+            # and so we do not need to check any other
             return Evaluator.TWO_OF_A_KIND
+
         elif len(code) == 3:
-            # possible: TWO_OF_A_KIND_TWICE, THREE_OF_A_KIND
-            if any(code[i][1] == 3 for i in range(3)):
+            # To have three different values, either two and two values are
+            # same, or three are same
+            if any(x == 3 for x in code.values()):
                 return Evaluator.THREE_OF_A_KIND
             else:
                 return Evaluator.TWO_OF_A_KIND_TWICE
+
         elif len(code) == 2:
-            # possible: FULL_HOUSE, FOUR_OF_A_KIND
-            if code[0] == (1, 4):  # FOUR_ONES
-                return Evaluator.FOUR_ONES
-            elif line == [1, 1, 1, 13, 13]:  # FULL_HOUSE_1_1_1_13_13
+            # The only possibility here is FULL_HOUSE or four of a kind, both of
+            # must be checked, and special case (four 1s) must be handled, same
+            # for <1, 1, 1, 13, 13> combination
+            for key, value in code.items():
+                if value == 4:
+                    return Evaluator.FOUR_ONES if key == 1 \
+                        else Evaluator.FOUR_OF_A_KIND
+            if 1 in code and 13 in code and code[1] == 3:
                 return Evaluator.FULL_HOUSE_1_1_1_13_13
-            elif code[0][1] == 4 or code[1][1] == 4:  # FOUR_OF_A_KIND
-                return Evaluator.FOUR_OF_A_KIND
-            else:  # FULL_HOUSE left
-                return Evaluator.FULL_HOUSE
+            return Evaluator.FULL_HOUSE
+
         else:
-            raise ValueError("Unknown input")
+            # Note that we can't have more different values than five (length
+            # of the line) and less than two (max four numbers of a kind), thus
+            # if we get here, there is a mistake
+            raise ValueError(f"Unknown combination of values: {line}")
 
     @staticmethod
     def _evaluate_diagonals(board: list) -> int:
         """
-        Calculates the score over two main diagonals
+        Calculates the score over two main diagonals.
+
         :param board: game state
         :return: score of diagonals based on constants
         """
@@ -105,16 +116,16 @@ class Evaluator:
     def evaluate(board: Union[list, Board]) -> int:
         """
         Calculates the score over the whole board.
+
         :param board: game plan
         :return: result score
         """
         if isinstance(board, Board):
             board = board.get()
         if any(any(x is None for x in row) for row in board):
-            raise ValueError("Board must not have empty cells")
+            raise ValueError(f"Board must not have empty cells {board}")
         score = 0
-        board_copy = deepcopy(board)
-        for row in board_copy:
+        for row in board:
             score += Evaluator.evaluate_line(row)
         board_copy = list(map(list, zip(*board)))
         for col in board_copy:
