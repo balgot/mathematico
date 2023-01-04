5eb08a9bea524aa987ec94a22bfbd3191e3fb529
michal <michal.barnisin14@gmail.com>
2023-01-04 23:38:20 +0100

    fix mypy options

--- game/mathematico/players/_human_player.py
+++ game/mathematico/players/_human_player.py
@@ -14,10 +14,9 @@ class HumanPlayer(Player):
         print(self.board)
         print(f"Next card:\t{number}")
 
-        row, col = None, None
+        row, col = -1, -1
         moves = set(self.board.possible_moves())
         while (row, col) not in moves:
             row = int(input(f"Row number [0, {self.board.size}):\t"))
             col = int(input(f"Column number [0, {self.board.size}):\t"))
-        assert row is not None and col is not None
         self.board.make_move((row, col), number)
